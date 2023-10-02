c1a4062bae2231b95b1693520a5c6a0ffa28ea56
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-10-03 02:12:38 +0530

    Initialize app

---
+++ Data/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AED_BE.Data
+{
+    public class DatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string DatabaseName { get; set; }
+        public string Connection { get; set; }
+    }
+}
