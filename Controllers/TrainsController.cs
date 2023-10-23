2200fdcc30df26f0e7ac45acd2a31ecaebc58cd2
Susith Rupasinghe <susith03program@gmail.com>
2023-10-23 22:17:53 +0530

    Train List added

--- Controllers/TrainsController.cs
+++ Controllers/TrainsController.cs
@@ -32,6 +32,32 @@ namespace AED_BE.Controllers
             return await _trainService.GetAllTrains();
         }
 
+
+        [HttpGet("list")]
+        public async Task<ActionResult<Trains>> GetTrainList() // Get one train by number
+        {
+            HashSet<string> trainList = new HashSet<string>();
+
+            List<Trains> trains = await _trainService.GetAllTrains();
+
+            for (int i = 0; i < trains.Count; i++) {
+
+                List<Stations> stations = trains[i].Stations;
+
+                for (int j = 0; j < stations.Count; j++) {
+                    trainList.Add(stations[j].Station.ToString());
+                }
+
+            }
+
+            if (trains == null)
+            {
+                return NotFound();
+            }
+           
+            return Ok(trainList);
+        }
+
         [HttpGet("{trainNumber}")]
         public async Task<ActionResult<Trains>> GetOneTrainByNumber(int number) // Get one train by number
         {
